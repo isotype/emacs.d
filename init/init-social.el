a47cad98720600dc0285849c6c1d982445e24ee6
Anton Strilchuk <anton@isoty.pe>
2014-03-24 20:41:37 +0000

    config for emacs social networking

---
+++ init/init-social.el
@@ -0,0 +1,31 @@
+;;; -*- mode: Emacs-Lisp; tab-width: 2; indent-tabs-mode:nil; -*-  ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
+;;; URL: http://isoty.pe                                           ;;;
+;;; Created: 24-03-2014                                            ;;;
+;;; Last-Updated: 24-03-2014                                       ;;;
+;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
+;;;                                                                ;;;
+;;; Filename: init-social                                          ;;;
+;;; Description: Setup for social networks                         ;;;
+;;;                                                                ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; TWITTER
+;; Configurations for twittering-mode
+(require 'twittering-mode)
+(setq
+ twittering-use-master-password t ; write to ~/.twittering-mode.gpg
+ twittering-icon-mode t
+ twittering-use-icon-storage t ; store icons to allow quicker loading
+ twittering-enable-unread-status-notifier t ; unread status in mode line
+)
+
+;; Spell check tweets
+(add-hook 'twittering-edit-mode-hook
+          (lambda ()
+            (ispell-minor-mode)
+            (flyspell-mode)))
+
+(provide 'init-social)
