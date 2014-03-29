42cae82ebc8dd1fdaddf7d86231e9268d3a57f45
Anton Strilchuk <anton@isoty.pe>
2014-03-29 01:09:07 +0000

    moved to individual init files

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
 ;;; URL: http://isoty.pe                                           ;;;
 ;;; Created: 23-03-2014                                            ;;;
-;;; Last-Updated: 24-03-2014                                       ;;;
+;;; Last-Updated: 29-03-2014                                       ;;;
 ;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
 ;;;                                                                ;;;
 ;;; Filename: init                                                 ;;;
@@ -56,27 +56,11 @@
 (set-language-environment 'utf-8)
 (prefer-coding-system 'utf-8)
 
-;;Deletes selected text when typing
-(delete-selection-mode t)
-(transient-mark-mode t)
-
 ;;Keep a list of recently opened files
 (require 'recentf)
 (setq recentf-max-saved-items 200
       recentf-max-menu-items 15)
 (recentf-mode +1)
-
-;;Winner Mode
-(winner-mode 1)
-
-;;Auto-insert/close bracket pairs
-(electric-pair-mode 1)
-
-;;Highlight matching parens
-(show-paren-mode 1)
-
-;;Hightlight entire bracket expression
-(setq show-paren-style 'expression)
 
 ;;Show current time
 (display-time-mode 1)
@@ -110,44 +94,9 @@
 ;;Don't show startup screen
 (setq inhibit-startup-screen t)
 
-;;IDO Search
-(require 'flx-ido)
-(ido-mode t)
-(ido-everywhere t)
-(setq ido-enable-flex-matching t
-      ido-use-filename-at-point nil
-      ido-auto-merge-work-directories-length 0
-      ido-use-virtual-buffers t)
-
-;;flx-ido completion system
-(flx-ido-mode 1)
-;; disable ido faces to see flx highlights.
-(setq ido-use-faces nil)
-(setq flx-ido-threshhold 10000)
-
-(require 'ido-vertical-mode)
-(ido-vertical-mode 1)
-(setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
-
-;; Use ido everywhere
-(require 'ido-ubiquitous)
-(ido-ubiquitous-mode 1)
-(setq ido-ubiquitous-use-new-completing-read 'webjump)
-
-(defun recentf-ido-find-file ()
-  "Find a recent file using ido."
-  (interactive)
-  (if (and ido-use-virtual-buffers (fboundp 'ido-toggle-virtual-buffers))
-       (ido-switch-buffer)
-     (find-file (ido-completing-read "Open file: " recentf-list nil t))))
-(global-set-key (kbd "C-c C-f") 'recentf-ido-find-file)
-
-;;ibuffer
-(require 'init-ibuffer)
-
-;;SMEX M-x IDO
-(require 'smex)
-(smex-initialize)
+;;Search Modes
+(require 'init-search)
+(require 'init-ido)
 
 ;;Drag Stuff is a minor mode for Emacs that makes
 ;;it possible to drag stuff (words, region, lines) around in Emacs
@@ -161,11 +110,15 @@
 
 ;;Sauron events tracking
 (require 'sauron)
-(setq sauron-separate-frame nil)
-(setq sauron-hide-mode-line t)
-;;Sauron window appear on every (virtual) desktop
-(setq sauron-sticky-frame t)
-(setq sauron-dbus-cookie t)
+(setq sauron-separate-frame nil
+      sauron-hide-mode-line t
+      sauron-sticky-frame t ;Sauron window appear on every (virtual) desktop
+      sauron-dbus-cookie t
+      sauron-max-line-length 120)
+(sauron-start)
+
+(global-set-key (kbd "C-c s") 'sauron-toggle-hide-show)
+(global-set-key (kbd "C-c t") 'sauron-clear)
 
 ;;Load GTAGS for getting tags from source files
 (setq load-path (cons "/usr/local/Cellar/global/6.2.9/share/gtags/" load-path))
@@ -332,11 +285,6 @@
 (require 'wc-mode)
 ;; Suggested setting
 (global-set-key "\C-cw" 'wc-mode)
-
-;;Undo Tree
-;;http://ergoemacs.org/emacs/emacs_best_redo_mode.html
-(require 'undo-tree)
-(global-undo-tree-mode 1)
 
 ;;Rebox2 Test
 (setq rebox-style-loop '(23 223 26))
