86e9406718f3d755cedeca803aa60532d9eca44f
Anton Strilchuk <anton@isoty.pe>
2014-03-29 01:31:42 +0000

    set to dark theme default

--- init/init-appearance.el
+++ init/init-appearance.el
@@ -3,7 +3,7 @@
 ;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
 ;;; URL: http://isoty.pe                                           ;;;
 ;;; Created: 24-03-2014                                            ;;;
-;;; Last-Updated: 24-03-2014                                       ;;;
+;;; Last-Updated: 29-03-2014                                       ;;;
 ;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
 ;;;                                                                ;;;
 ;;; Filename: init-appearance                                      ;;;
@@ -13,7 +13,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;Default theme
-(light)
+(dark)
 
 ;;; Wrap Text
 (global-visual-line-mode 1)
