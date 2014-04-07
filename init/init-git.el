b44bdf09f297b9459905623ca34616535c3abc8f
Anton Strilchuk <anton@isoty.pe>
2014-04-07 23:26:51 +0100

    change git-svn search project keybinding to H-z, Ag search H-q

--- init/init-git.el
+++ init/init-git.el
@@ -1,56 +1,16 @@
-;;; init-git.el --- 
-;; 
-;; Filename: init-git.el
-;; Description: 
-;; Author: y_pe
-;; Maintainer: 
-;; Created: Sun Feb 16 19:18:34 2014 (+0000)
-;; Version: 
-;; Package-Requires: ()
-;; Last-Updated: Wed Mar 19 09:27:59 2014 (+0000)
-;;           By: anton
-;;     Update #: 15
-;; URL: isoty.pe
-;; Doc URL: https://github.com/purcell/emacs.d/tree/master/lisp
-;; Keywords: git, git-gutter, magit
-;; Compatibility: Emacs 24.3.50.1+
-;; 
+;;; -*- mode: Emacs-Lisp; tab-width: 2; indent-tabs-mode:nil; -*-  ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;;  This is a modified version of Steve Purcell's init-git.el
-;;  which can be found at the Doc URL. I have made slight changes
-;;  one of which is the addition of Peter Seibel's use of
-;;  git-gutter-fringe in his emacs.d configuration.
-;; 
-;; 
+;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
+;;; URL: http://isoty.pe                                           ;;;
+;;; Created: 07-04-2014                                            ;;;
+;;; Last-Updated: 07-04-2014                                       ;;;
+;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
+;;;                                                                ;;;
+;;; Filename: init-git                                             ;;;
+;;; Description: Git Setup                                         ;;;
+;;;                                                                ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Change Log:
-;;  - Added git-gutter-fringe
-;;  - Set color preferences for git-gutter
-;;  - Removed Git-wip due to onslaught of file load errors
-;; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3, or
-;; (at your option) any later version.
-;; 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;; 
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
-;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Code:
 (require 'magit)
 (require 'git-gutter-fringe)
 (require 'magit-blame)
@@ -85,9 +45,12 @@
   (fullframe magit-status magit-mode-quit-window))
 
 ;; When we start working on git-backed files, use git-wip if available
-;; (after-load 'vc-git
-;; 	    (global-magit-wip-save-mode 1)
-;; 	    (diminish 'magit-wip-save-mode))
+(after-load 'magit
+  (global-magit-wip-save-mode 1)
+  (diminish 'magit-wip-save-mode))
+
+(after-load 'magit
+  (diminish 'magit-auto-revert-mode))
 
 ;; Use the fringe version of git-gutter
 (after-load 'git-gutter
@@ -103,7 +66,7 @@
     (add-hook 'magit-mode-hook (lambda () (local-unset-key [(meta h)])))))
 
 ;; Convenient binding for vc-git-grep
-(global-set-key (kbd "C-x v f") 'vc-git-grep)
+(global-set-key (kbd "H-z") 'vc-git-grep)
 
 ;; git-svn support
 (require 'magit-svn)
@@ -112,7 +75,7 @@
 
 (after-load 'compile
   (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
-		      '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
+                      '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
     (add-to-list 'compilation-error-regexp-alist-alist defn)
     (add-to-list 'compilation-error-regexp-alist (car defn))))
 
@@ -132,7 +95,7 @@
                      (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
 
 (require 'git-messenger)
-(global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
+(global-set-key (kbd "H-p") #'git-messenger:popup-message)
 
 ;; github
 (require 'yagist)
