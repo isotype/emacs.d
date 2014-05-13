1d8dcc7a92375761fa4b668b379cdb08bde62ee9
Anton Strilchuk <anton@isoty.pe>
2014-05-13 13:44:41 +0100

    TeX bugjiggery hackery to get the damn thing to work right

--- init/init-latex.el
+++ init/init-latex.el
@@ -1,64 +1,29 @@
-;;; init-latex.el --- 
-;; 
-;; Filename: init-latex.el
-;; Description: 
-;; Author: anton
-;; Maintainer: 
-;; Created: Tue Mar 18 23:46:59 2014 (+0000)
-;; Version: 
-;; Package-Requires: ()
-;; Last-Updated: Wed Mar 19 00:11:25 2014 (+0000)
-;;           By: anton
-;;     Update #: 7
-;; URL: 
-;; Doc URL: 
-;; Keywords: 
-;; Compatibility: 
-;; 
+;;; -*- mode: Emacs-Lisp; tab-width: 2; indent-tabs-mode:nil; -*-  ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
+;;; URL: http://isoty.pe                                           ;;;
+;;; Created: 24-04-2014                                            ;;;
+;;; Last-Updated: 07-05-2014                                       ;;;
+;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
+;;;                                                                ;;;
+;;; Filename: init-latex                                           ;;;
+;;; Version:                                                       ;;;
+;;; Description:                                                   ;;;
+;;;                                                                ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Change Log:
-;; 
-;; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3, or
-;; (at your option) any later version.
-;; 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;; 
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
-;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Code:
-
-(require 'tex-site) ;; AUCTeX
+(require 'tex-site) ; AUCTeX
 (require 'flymake)
 (require 'auto-complete-auctex)
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 (setq TeX-save-query nil)
-(setq TeX-PDF-mode t) ;; Compile to PDF by default
+(setq TeX-PDF-mode t) ; Compile to PDF by default
 
 (defun flymake-get-tex-args (file-name)
-(list "pdflatex"
-(list "-file-line-error" "-draftmode" "-interaction=nonstopmode" file-name)))
+  (list "xelatex"
+        (list "-shell-escape" "-draftmode" "-interaction nonstopmode" file-name "&& bibtex %f")))
 
 (add-hook 'LaTeX-mode-hook 'flymake-mode)
 
@@ -72,8 +37,88 @@
 (add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
 (add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
 
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
+(add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
+
 (setq TeX-view-program-list
-     '(("PDF Viewer" "/opt/homebrew-cask/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+      '(("PDF Viewer" "/opt/homebrew-cask/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+
+;;,-=========-
+;;| Org Latex
+;;`-=========-
+(require 'ob-latex)
+(require 'ox-latex)
+(require 'ox-html)
+(require 'org-mac-link)
+
+(define-key org-mode-map (kbd "C-'") 'org-mac-grab-link)
+
+(setq org-export-with-sub-superscripts '{})
+(setq org-export-with-smart-quotes t)
+
+(setq org-html-preamble nil)
+(setq org-html-postamble nil)
+(setq org-html-doctype "html5")
+(setq org-html-html5-fancy t)
+(setq org-html-head-extra (concat
+                           "<style type=\"text/css\">\n"
+                           "body { font-family: sans-serif; font-size: 1em; line-height: 1.5; color: #222222 }"
+                           "table { border: 1px solid black; }"
+                           "td { padding: 2px 4px; }"
+                           "</style>\n"))
+
+(setq org-html-mathjax-options (quote (
+                                       (path "http://cdn.mathjax.org/mathjax/latest/MathJax.js")
+                                       (scale "100")
+                                       (align "center")
+                                       (indent "2em")
+                                       (mathml nil))))
+
+(add-to-list 'org-latex-packages-alist '("" "array"))
+(add-to-list 'org-latex-packages-alist '("" "booktabs"))
+(add-to-list 'org-latex-packages-alist '("stretch=10" "microtype"))
+(add-to-list 'org-latex-packages-alist '("" "lmodern"))
+
+;; use xelatex
+(setq org-latex-pdf-process
+      '("xelatex -shell-escape -interaction nonstopmode %f; bibtex %f; xelatex -shell-escape -interaction nonstopmode %f; xelatex -shell-escape -interaction nonstopmode %f"))
+
+(setq org-latex-listings 'minted
+      org-latex-minted-options
+      '(("mathescape" "")
+        ("linenos" "")
+        ("frame" "lines")
+        ("fontsize" "\\scriptsize")))
+(add-to-list 'org-latex-classes
+             '("ypaper"
+               "\\documentclass[11pt,a4paper,titlepage]{article}
+\\usepackage{fontspec}
+\\setmainfont{Droid Serif}
+\\setsansfont{Droid Sans}
+\\setmonofont{Droid Sans Mono}
+\\usepackage{graphicx}
+\\usepackage{hyperref}
+\\usepackage{minted}
+\\usemintedstyle{github}
+
+\\usepackage{enumitem}
+\\setitemize{noitemsep}
+\\setlist[itemize,1]{label=\\textbullet}
+\\setlist[itemize,2]{label=\\textopenbullet}
+\\setlist[itemize,3]{label=\\textbullet}
+\\setlist[itemize,4]{label=\\textopenbullet}
+
+\\usepackage{geometry}
+\\geometry{a4paper, textwidth=6.5in, textheight=10in,
+            marginparsep=7pt, marginparwidth=.6in}
+
+[NO-PACKAGES]
+[NO-DEFAULT-PACKAGES]"
+("\\section{%s}" . "\\section*{%s}")
+("\\subsection{%s}" . "\\subsection*{%s}")
+("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+("\\paragraph{%s}" . "\\paragraph*{%s}")
+("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 (provide 'init-latex)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
