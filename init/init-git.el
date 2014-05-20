3bda2442096f7d2d0c72e2637e3f724484a359c8
Anton Strilchuk <anton@isoty.pe>
2014-05-20 01:26:11 +0100

    add gh package

--- init/init-git.el
+++ init/init-git.el
@@ -3,7 +3,7 @@
 ;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
 ;;; URL: http://isoty.pe                                           ;;;
 ;;; Created: 07-04-2014                                            ;;;
-;;; Last-Updated: 13-05-2014                                       ;;;
+;;; Last-Updated: 20-05-2014                                       ;;;
 ;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
 ;;;                                                                ;;;
 ;;; Filename: init-git                                             ;;;
@@ -19,6 +19,7 @@
 (require 'gitignore-mode)
 (require 'gitconfig-mode)
 (require 'git-messenger)
+(require 'gh)
 
 (setq-default
  magit-save-some-buffers nil
@@ -95,12 +96,11 @@
 (global-set-key (kbd "H-p") #'git-messenger:popup-message)
 
 ;; github
-(after-load
-  (require 'gist) ; for gist-list
-  (require 'yagist) ; for yagist-region-or-buffer...
-  (global-set-key (kbd "C-M-s-<f1>") 'yagist-region-or-buffer-private)
-  (global-set-key (kbd "C-M-s-<f2>") 'yagist-region-or-buffer)
-  (global-set-key (kbd "C-M-s-z") 'gist-list))
+(require 'gist) ; for gist-list
+(require 'yagist) ; for yagist-region-or-buffer...
+(global-set-key (kbd "C-M-s-<f1>") 'yagist-region-or-buffer-private)
+(global-set-key (kbd "C-M-s-<f2>") 'yagist-region-or-buffer)
+(global-set-key (kbd "C-M-s-z") 'gist-list)
 
 (require 'github-browse-file)
 (require 'bug-reference-github)
