7a998236f5c70b9ae192d5314e8adf658c8664a2
Anton Strilchuk <anton@isoty.pe>
2014-05-20 01:25:19 +0100

    added some nifty purcell bits i liked

--- init/init-dash.el
+++ init/init-dash.el
@@ -1,50 +1,17 @@
-;;; init-dash.el --- 
-;; 
-;; Filename: init-dash.el
-;; Description: Helm Dash Configuration
-;; Author: y_pe
-;; Maintainer: y_pe
-;; Created: Fri Mar 21 17:45:26 2014 (+0000)
-;; Package-Requires: (Dash.app, helm-dash)
-;; Last-Updated: Fri Mar 21 18:08:20 2014 (+0000)
-;;           By: y_pe
-;;     Update #: 6
-;; URL: github/isotype/emacs.d
-;; Doc URL: http://isoty.pe
-;; Keywords: Dashapp, helm, docsets
-;; Compatibility: Emacs 24++, OSX
-;; 
+;;; -*- mode: Emacs-Lisp; tab-width: 2; indent-tabs-mode:nil; -*-  ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;;  - Initialization of modes to use with helm-dash docsets
-;; 
-;; 
+;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
+;;; URL: http://isoty.pe                                           ;;;
+;;; Created: 20-05-2014                                            ;;;
+;;; Last-Updated: 20-05-2014                                       ;;;
+;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
+;;;                                                                ;;;
+;;; Filename: init-dash                                            ;;;
+;;; Version:                                                       ;;;
+;;; Description:                                                   ;;;
+;;;                                                                ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Change Log:
-;;  - Added elisp-doc, and clisp-doc
-;; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3, or
-;; (at your option) any later version.
-;; 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;; 
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
-;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Code:
 (require 'helm-dash)
 
 (defun elisp-doc()
@@ -56,6 +23,23 @@
   (interactive)
   (setq-local helm-dash-docset '("Common Lisp")))
 (add-hook 'common-lisp-mode 'clisp-doc)
+
+(defun sanityinc/dash-installed-p ()
+  "Return t if Dash is installed on this machine, or nil otherwise."
+  (let ((lsregister "/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister"))
+    (and (file-executable-p lsregister)
+         (not (string-equal
+               ""
+               (shell-command-to-string
+                (concat lsregister " -dump|grep com.kapeli.dash")))))))
+
+(when (and *is-a-mac* (not (package-installed-p 'dash-at-point)))
+  (message "Checking whether Dash is installed")
+  (when (sanityinc/dash-installed-p)
+    (require 'dash-at-point)))
+
+(when (package-installed-p 'dash-at-point)
+  (global-set-key (kbd "C-c d") 'dash-at-point))
 
 (provide 'init-dash)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
