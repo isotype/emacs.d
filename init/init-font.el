b5460cc3e999a427bdeb428188c0a6b2dab7adbe
Anton Strilchuk <anton@isoty.pe>
2014-07-26 20:11:23 +0100

    PT Mono anyone?

--- init/init-font.el
+++ init/init-font.el
@@ -3,7 +3,7 @@
 ;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
 ;;; URL: http://isoty.pe                                           ;;;
 ;;; Created: 08-04-2014                                            ;;;
-;; Last-Updated: 03-06-2014                                         ;;
+;; Last-Updated: 25-07-2014                                         ;;
 ;;   By: Anton Strilchuk <ype@env.sh>                               ;;
 ;;;                                                                ;;;
 ;;; Filename: init-font                                            ;;;
@@ -60,6 +60,6 @@ by the :height face attribute."
 (global-set-key (kbd "H-\=") 'ype/increase-default-font-height)
 (global-set-key (kbd "H-\-") 'ype/decrease-default-font-height)
 
-(set-face-attribute 'default nil :font "Droid Sans Mono-14")
+(set-frame-font "PT Mono" 16 t)
 
 (provide 'init-font)
