781bece1f258396f4ccf1b336eaddae35d8f6d5b
Anton Strilchuk <anton@isoty.pe>
2014-07-26 20:12:13 +0100

    so i don't wreck my wrists

--- init/init-rsi.el
+++ init/init-rsi.el
@@ -3,21 +3,21 @@
 ;; Author: Anton Strilchuk <ype@env.sh>                             ;;
 ;; URL: http://ype.env.sh                                           ;;
 ;; Created: 02-06-2014                                              ;;
-;; Last-Updated: 06-06-2014                                         ;;
+;; Last-Updated: 10-06-2014                                         ;;
 ;;   By: Anton Strilchuk <ype@env.sh>                               ;;
 ;;                                                                  ;;
 ;; Filename: init-rsi                                               ;;
 ;; Version:                                                         ;;
 ;; Description: type-break mode config                              ;;
-;; Depends on: init-edit-utils (s2m function)                       ;;
+;; Depends on: init-edit-utils (m2s function)                       ;;
 ;; Package Requires: ()                                             ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq type-break-file-name "~/.emacs.d/.type-break"
-      type-break-interval (s2m 20)
-      type-break-good-rest-interval (s2m 5)
+      type-break-interval (m2s 20)
+      type-break-good-rest-interval (m2s 5)
       type-break-keystroke-threshold '(1 . 8000)
-      type-break-time-warning-intervals '((s2m 5) (s2m 10) (s2m 15))
+      type-break-time-warning-intervals '((m2s 5) (m2s 10) (m2s 15))
       type-break-demo-functions '(type-break-demo-boring)
       type-break-mode-line-message-mode t
       type-break-terse-messages t)
