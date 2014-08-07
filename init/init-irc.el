0d64f1a3e6b00496fa419b4b299e780eb8003cea
Anton Strilchuk <anton@isoty.pe>
2014-08-07 11:05:13 +0100

    added macro for connecting through tls/ssl

--- init/init-irc.el
+++ init/init-irc.el
@@ -3,8 +3,8 @@
 ;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
 ;;; URL: http://isoty.pe                                           ;;;
 ;;; Created: 15-04-2014                                            ;;;
-;;; Last-Updated: 25-05-2014                                       ;;;
-;;;   By: Anton Strilchuk <anton@isoty.pe>                         ;;;
+;; Last-Updated: 07-08-2014                                         ;;
+;;   By: Anton Strilchuk <ype@env.sh>                               ;;
 ;;;                                                                ;;;
 ;;; Filename: init-irc                                             ;;;
 ;;; Version:                                                       ;;;
@@ -44,7 +44,6 @@
 ;; wrap lines to buffer when typing
 (add-hook 'erc-mode-hook 'visual-line-mode)
 
-
 (setq erc-timestamp-only-if-changed-flag nil ;; always timestamp
       erc-timestamp-format "%H:%M:%S "
       erc-fill-prefix nil ;; don't force indentation
@@ -73,5 +72,20 @@
          (set-buffer buffer)
          (when (eq major-mode 'erc-mode)
            (setq erc-fill-column (- (window-width w) 2)))))))))
+
+;; From: Emacs Wiki
+;; Link: http://bit.ly/1qZL2Wp
+(defmacro asf-erc-bouncer-connect (command server port nick ssl pass)
+   "Create interactive command `command', for connecting to an IRC server. The
+   command uses interactive mode if passed an argument."
+   (fset command
+         `(lambda (arg)
+           (interactive "p")
+     (if (not (= 1 arg))
+         (call-interactively 'erc)
+       (let ((erc-connect-function ',(if ssl
+                 'erc-open-ssl-stream
+               'open-network-stream)))
+         (erc :server ,server :port ,port :nick ,nick :password ,pass))))))
 
 (provide 'init-irc)
