0bd4f1efb3f6f05beed26be53a77c45207c3b262
Anton Strilchuk <anton@isoty.pe>
2014-08-11 17:57:22 +0100

    Option to require after submodule is loaded

--- init/init-git-submodules.el
+++ init/init-git-submodules.el
@@ -3,8 +3,8 @@
 ;; Author: Anton Strilchuk <ype@env.sh>                             ;;
 ;; URL: http://ype.env.sh                                           ;;
 ;; Created: 08-08-2014                                              ;;
-;; Last-Updated: 08-08-2014                                         ;;
-;;  Update #: 6                                                     ;;
+;; Last-Updated: 11-08-2014                                         ;;
+;;  Update #: 11                                                    ;;
 ;;   By: Anton Strilchuk <ype@env.sh>                               ;;
 ;;                                                                  ;;
 ;; Filename: init-git-submodules                                    ;;
@@ -15,7 +15,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (setq git-submodules-directory "/Users/anton/.emacs.d/submodules/" )
-(defun require-git-submodule (submodule)
-  (add-to-list 'load-path (concat git-submodules-directory (symbol-name submodule) "/")))
+(defun require-git-submodule (submodule &optional add-a-require)
+  "Add Package from Git-Submodules-Directory and optional require package"
+  (interactive)
+  (add-to-list 'load-path (concat git-submodules-directory (symbol-name submodule) "/"))
+  (if add-a-require
+      (progn
+        (let ((symodule submodule))
+          (require symodule)))
+    (progn
+      nil)))
 
 (provide 'init-git-submodules)
