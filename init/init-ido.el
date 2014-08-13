f2b234a852238ab997b915c8f0b87e1cac07067a
Anton Strilchuk <anton@isoty.pe>
2014-08-13 13:50:08 +0100

    Fixes to key sequence for bookmark

--- init/init-ido.el
+++ init/init-ido.el
@@ -48,27 +48,27 @@
 ;; Bookmark
 (require 'bookmark)
 
-(after-load 'bookmark
-  ;; Add Bookmark list to ido
-  (setq enable-recursive-minibuffers t)
-  (define-key ido-file-dir-completion-map [(control ?')] 'ido-goto-bookmark)
-  (defun ido-goto-bookmark (bookmark)
-    (interactive
-     (list (bookmark-completing-read "Jump to bookmark" bookmark-current-bookmark)))
-    (unless bookmark
-      (error "No bookmark specified"))
-    (let ((filename (bookmark-get-filename bookmark)))
-      (if (file-directory-p filename)
-          (progn
-            (ido-set-current-directory filename)
-            (setq ido-text ""))
+
+;; Add Bookmark list to ido
+(setq enable-recursive-minibuffers t)
+(define-key ido-file-dir-completion-map [(meta control ?b)] 'ido-goto-bookmark)
+(defun ido-goto-bookmark (bookmark)
+  (interactive
+   (list (bookmark-completing-read "Jump to bookmark" bookmark-current-bookmark)))
+  (unless bookmark
+    (error "No bookmark specified"))
+  (let ((filename (bookmark-get-filename bookmark)))
+    (if (file-directory-p filename)
         (progn
-          (ido-set-current-directory (file-name-directory filename))))
-      (setq ido-exit 'refresh
-            ido-text-init ido-text
-            ido-rotate-temp t)
-      (exit-minibuffer)))
-  (global-set-key (kbd "C-M-<return>") 'bookmark-set))
+          (ido-set-current-directory filename)
+          (setq ido-text ""))
+      (progn
+        (ido-set-current-directory (file-name-directory filename))))
+    (setq ido-exit 'refresh
+          ido-text-init ido-text
+          ido-rotate-temp t)
+    (exit-minibuffer)))
+(global-set-key (kbd "C-M-<return>") 'bookmark-set)
 
 ;;ibuffer
 (require 'init-ibuffer)
