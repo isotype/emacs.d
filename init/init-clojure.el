2fb283722b08ce2a456f37df075e5098e54f5a70
Anton Strilchuk <anton@env.sh>
2014-08-21 17:41:43 +0100

    Hotfix: dealing with various bugs

--- init/init-clojure.el
+++ init/init-clojure.el
@@ -3,7 +3,7 @@
 ;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
 ;;; URL: http://isoty.pe                                           ;;;
 ;;; Created: 12-04-2014                                            ;;;
-;; Last-Updated: 13-08-2014                                         ;;
+;; Last-Updated: 18-08-2014                                         ;;
 ;;   By: Anton Strilchuk <ype@env.sh>                               ;;
 ;;;                                                                ;;;
 ;;; Filename: init-clojure                                         ;;;
@@ -70,6 +70,9 @@
 
 ;; Learning
 ;;(require-git-submodule '4clj-el)
+
+;; Trace
+(require-package 'cider-tracing)
 
 (provide 'init-clojure)
 ;;; init-clojure.el ends here
