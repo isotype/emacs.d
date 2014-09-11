bf8a599b6c65e24463e3ab907ee857730e3005cb
Anton Strilchuk <anton@env.sh>
2014-09-11 12:23:53 +0100

    Added ruby-mode support

--- init/init-paredit.el
+++ init/init-paredit.el
@@ -3,7 +3,7 @@
 ;;; Author: Anton Strilchuk <anton@isoty.pe>                       ;;;
 ;;; URL: http://isoty.pe                                           ;;;
 ;;; Created: 03-04-2014                                            ;;;
-;; Last-Updated: 04-08-2014                                         ;;
+;; Last-Updated: 07-09-2014                                         ;;
 ;;   By: Anton Strilchuk <ype@env.sh>                               ;;
 ;;;                                                                ;;;
 ;;; Filename: init-paredit                                         ;;;
