a0fe6223366718301c556f92f4c1d0b171bd60af
Anton Strilchuk <anton@env.sh>
2014-10-03 09:13:35 +0100

    Added ein for iPython Notebook

--- init/init-python.el
+++ init/init-python.el
@@ -3,8 +3,8 @@
 ;; Author: Anton Strilchuk <anton@ilyfa.cc>                         ;;
 ;; URL: http://ype.env.sh                                           ;;
 ;; Created: 23-07-2014                                              ;;
-;; Last-Updated: 12-08-2014                                         ;;
-;;  Update #: 5                                                     ;;
+;; Last-Updated: 01-10-2014                                         ;;
+;;  Update #: 19                                                    ;;
 ;;   By: Anton Strilchuk <ype@env.sh>                               ;;
 ;;                                                                  ;;
 ;; Filename: init-python                                            ;;
@@ -28,11 +28,19 @@
 ;;|  Anaconda: Code navigation, documentation lookup and completion for Python
 ;;|  Link: [[https://github.com/proofit404/anaconda-mode][proofit404/anaconda-mode]]
 ;;`---------------------------------------------------------------------------
-
 (require-package 'anaconda-mode)
 
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'eldoc-mode)
 
+
+;;,-----------------------
+;;| Emacs iPython Notebook
+;;`-----------------------
+(require-package 'ein)
+(setq ein:use-auto-complete t)
+(setq ein:use-smartrep nil)
+
+
 (provide 'init-python)
 ;;; init-python.el ends here
