88d96aa338cd73c6b4849485564192a2d55333cc
Anton Strilchuk <anton@env.sh>
2014-11-01 11:52:25 +0000

    Polymode for R

---
+++ init/init-polymode.el
@@ -0,0 +1,19 @@
+;; -*- mode: Emacs-Lisp; tab-width: 2; indent-tabs-mode:nil; -*-    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Author: Anton Strilchuk <ype@env.sh>                             ;;
+;; URL: http://ype.env.sh                                           ;;
+;; Created: 01-10-2014                                              ;;
+;; Last-Updated: 01-10-2014                                         ;;
+;;  Update #: 2                                                     ;;
+;;   By: Anton Strilchuk <ype@env.sh>                               ;;
+;;                                                                  ;;
+;; Filename: init-polymode                                          ;;
+;; Version:                                                         ;;
+;; Description: Literate Coding with Polymode                       ;;
+;;                                                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require-package 'polymode)
+(require 'poly-R)
+(require 'poly-markdown)
