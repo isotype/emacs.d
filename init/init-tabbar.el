65324f6cf5c9190037bfb475cdcfa02269f003c5
Anton Strilchuk <anton@env.sh>
2014-11-01 11:45:12 +0000

    Autohide Tabbars (Emacs v25.0.50.1)

--- init/init-tabbar.el
+++ init/init-tabbar.el
@@ -1,7 +1,6 @@
 ;;Tabs
 (require-package 'tabbar)
 (require-package 'tabbar-ruler)
-(tabbar-mode t) ;; turn on the tabbar
 (tabbar-install-faces t)
 
 ;; (defun tabbar-buffer-groups-by-dir ()
@@ -18,39 +17,115 @@
 ;;               (list "Misc"))
 ;;              (t (list dir))))))
 
+(defun autohide-tabbar ()
+  "Make tabbar briefly show itself while you are switching
+ buffers with shortcuts.  Tested with GNU Emacs 25.0.50.1."
+  (interactive)
+  (defvar *tabbar-autohide-delay* 3)
+  (tabbar-mode -1)
+  (defvar *tabbar-autohide-timer* nil)
+  (defun renew-tabbar-autohide-timer (seconds-to-show)
+    (if (timerp *tabbar-autohide-timer*)
+        (cancel-timer *tabbar-autohide-timer*))
+    (setf *tabbar-autohide-timer*
+          (run-with-timer
+           seconds-to-show nil
+           (lambda () (tabbar-mode -1)
+             (setf *tabbar-autohide-timer* nil)))))
+  (global-set-key
+   (kbd "H-]")
+   (lambda ()
+     (interactive)
+     (if (null tabbar-mode)
+         (progn
+           (tabbar-mode 1)
+           (tabbar-forward-tab))
+       (tabbar-forward-tab))
+     (renew-tabbar-autohide-timer 4)))
+  (global-set-key
+   (kbd "H-[")
+   (lambda ()
+     (interactive)
+     (if (null tabbar-mode)
+         (progn
+           (tabbar-mode 1)
+           (tabbar-backward-tab))
+       (tabbar-backward-tab))
+     (renew-tabbar-autohide-timer 4)))
+  (global-set-key
+   (kbd "H-}")
+   (lambda ()
+     (interactive)
+     (if (null tabbar-mode)
+         (progn
+           (tabbar-mode 1)
+           (tabbar-forward-group))
+       (tabbar-forward-group))
+     (renew-tabbar-autohide-timer 5)))
+  (global-set-key
+   (kbd "H-{")
+   (lambda ()
+     (interactive)
+     (if (null tabbar-mode)
+         (progn
+           (tabbar-mode 1)
+           (tabbar-backward-group))
+       (tabbar-backward-group))
+     (renew-tabbar-autohide-timer 5)))
+  (global-set-key
+   (kbd "H-1")
+   (lambda ()
+     (interactive)
+     (if (null tabbar-mode)
+         (progn
+           (tabbar-mode 1)
+           (tabbar-ruler-group-user-buffers))
+       (tabbar-ruler-group-user-buffers))
+     (renew-tabbar-autohide-timer 3)))
+  (global-set-key
+   (kbd "H-2")
+   (lambda ()
+     (interactive)
+     (if (null tabbar-mode)
+         (progn
+           (tabbar-mode 1)
+           (tabbar-ruler-group-by-projectile-project))
+       (tabbar-ruler-group-by-projectile-project))
+     (renew-tabbar-autohide-timer 3)))
+  (global-set-key
+   (kbd "H-\`")
+   (lambda ()
+     (interactive)
+     (if (null tabbar-mode)
+         (progn
+           (tabbar-mode 1)
+           (tabbar-ruler-move))
+       (tabbar-mode -1)))))
+
+(autohide-tabbar)
+
 (defun tabbar-switch-grouping-method (&optional arg)
   "Changes grouping method of tabbar to grouping by dir.
 With a prefix arg, changes to grouping by major mode."
   (interactive "P")
   (ignore-errors
     (if arg
-      (setq tabbar-buffer-groups-function 'tabbar-buffer-groups) ;; the default setting
-        (setq tabbar-buffer-groups-function 'tabbar-buffer-groups-by-dir))))
-
-(global-set-key (kbd "C-c C-\;") 'tabbar-ruler-group-buffer-groups)
-(global-set-key (kbd "C-c C-\'") 'tabbar-ruler-group-by-projectile-project)
-(global-set-key [(backtab)] 'tabbar-ruler-move)
+        (setq tabbar-buffer-groups-function 'tabbar-buffer-groups) ;; the default setting
+      (setq tabbar-buffer-groups-function 'tabbar-buffer-groups-by-dir))))
 
 (setq tabbar-add-tab-function 'tabbar-add-tab)
 (add-hook 'after-change-major-mode-hook 'tabbar-install-faces)
 
+;; Add a buffer modification state indicator in the tab label, and place a
+;; space around the label to make it looks less crowd.
+(defadvice tabbar-buffer-tab-label (after fixup_tab_label_space_and_flag activate)
+  (setq ad-return-value
+        (if (and (buffer-modified-p (tabbar-tab-value tab))
+               (buffer-file-name (tabbar-tab-value tab)))
+            (concat " [" (concat ad-return-value "] "))
+          (concat " " (concat ad-return-value " ")))))
 
-;;Buffer Switch Remap
-(global-set-key (kbd "H-[") 'tabbar-backward-tab)
-(global-set-key (kbd "H-]") 'tabbar-forward-tab)
-(global-set-key (kbd "H-{") 'tabbar-backward-group)
-(global-set-key (kbd "H-}") 'tabbar-forward-group)
-
- ;; Add a buffer modification state indicator in the tab label, and place a
- ;; space around the label to make it looks less crowd.
- (defadvice tabbar-buffer-tab-label (after fixup_tab_label_space_and_flag activate)
-   (setq ad-return-value
-         (if (and (buffer-modified-p (tabbar-tab-value tab))
-                  (buffer-file-name (tabbar-tab-value tab)))
-             (concat " [" (concat ad-return-value "] "))
-           (concat " " (concat ad-return-value " ")))))
-
-(tabbar-ruler-group-buffer-groups)
+(tabbar-ruler-group-user-buffers)
 
 
 (provide 'init-tabbar)
